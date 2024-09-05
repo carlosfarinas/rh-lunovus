258b35e66f8a3b9cde970c75e4dd36be32fd5c6a
Carlos Farinas <cfarinas@gmail.com>
2024-09-05 11:30:23 -0400

    First Commit

---
+++ app/Http/Controllers/GitHubUserSearch.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class GitHubUserSearch extends Controller
+{
+    const GITHUB_URL = 'https://api.github.com/users/';
+    public function getUserSearch($name)
+    {
+        $url_user = self::GITHUB_URL.$name;
+        $url_followers = self::GITHUB_URL.$name."/followers";
+        $res_user = Http::get($url_user);
+        $res_followers = Http::get($url_followers);
+
+        if ($res_user->failed()) {
+            return response()->json([
+                "status" => false,
+                "message" => "No user found",
+                "data" => []
+            ]);
+        } else {
+            return response()->json([
+                "status" => true,
+                "message" => "found",
+                "data" => [
+                    "user" => $res_user->json(),
+                    "followers" => $res_followers->json()
+                ]
+            ]);
+        }
+
+    }
+
+}
